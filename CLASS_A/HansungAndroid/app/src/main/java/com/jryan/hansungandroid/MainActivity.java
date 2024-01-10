195cbca6789b257121b8511e021ff2240e8ca911
jys-master <hansungtutoring@google.com>
2024-01-10 21:03:37 +0900

    [A-AndroidProejct] new commit

---
+++ CLASS_A/HansungAndroid/app/src/main/java/com/jryan/hansungandroid/MainActivity.java
@@ -0,0 +1,21 @@
+package com.jryan.hansungandroid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        WebView webview = findViewById(R.id.webview);
+        webview.setWebViewClient(new WebViewClient());
+        webview.getSettings().setJavaScriptEnabled(true);
+        webview.loadUrl("https://www.naver.com");
+    }
+}
