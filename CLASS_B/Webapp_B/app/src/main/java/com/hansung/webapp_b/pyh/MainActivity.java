5fa8396b4c2491565cd50f9a1da25d3df46bc5fe
yhwebhello <thistime0317@naver.com>
2024-01-14 21:27:38 +0900

    수정된 내용을 입력한다

---
+++ CLASS_B/Webapp_B/app/src/main/java/com/hansung/webapp_b/pyh/MainActivity.java
@@ -0,0 +1,21 @@
+package com.hansung.webappproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        WebView webview = findViewById(R.id.webview);
+        webview.setWebViewClient(new WebViewClient());
+        webview.getSettings().setJavaScriptEnabled(true);
+        webview.loadUrl("https://project-6954791464255453710.web.app/");
+    }
+}
